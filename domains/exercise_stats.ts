dcaaddea1f545708b788ad1ed4b72c1fec655fb6
Alec Skinner <skinner452@gmail.com>
2025-01-15 13:46:11 -0500

    Combine stats into one type for ease

--- domains/exercise_stats.ts
+++ domains/exercise_stats.ts
@@ -1,95 +1,58 @@
-import {
-  CardioExerciseStatItem,
-  CardioExerciseStats,
-  StrengthExerciseStatItem,
-  StrengthExerciseStats,
-} from "../types/exercise_stats";
+import { ExerciseStatItem, ExerciseStats } from "../types/exercise_stats";
 import { ExerciseTypeID } from "../types/exercise_types";
 import { DB } from "../utils/db";
 
-const strengthExerciseColumns = `weight, reps, sets, created_on`;
+const exerciseStatsColumns = `weight, reps, sets, distance, duration_seconds, incline, created_on`;
 
-const castToStrengthExerciseStatItem = (row: any) => {
+const strengthOrderBy = `weight DESC`;
+const cardioOrderBy = `distance DESC`;
+
+const castToExerciseStatItem = (row: any) => {
   return {
     weight: row.weight,
     reps: row.reps,
     sets: row.sets,
-    date: row.created_on,
-  } as StrengthExerciseStatItem;
-};
-
-const cardioExerciseColumns = `distance, duration_seconds, incline, created_on`;
-
-const castToCardioExerciseStatItem = (row: any) => {
-  return {
     distance: row.distance,
     durationSeconds: row.duration_seconds,
     incline: row.incline,
     date: row.created_on,
-  } as CardioExerciseStatItem;
-};
-
-const getStrengthExerciseStats = async (exerciseID: number) => {
-  const [bestRows] = await DB.query<any[]>(
-    `SELECT ${strengthExerciseColumns}
-    FROM workouts
-    WHERE exercise_id = ?
-    ORDER BY weight DESC
-    LIMIT 1`,
-    [exerciseID]
-  );
-
-  const [lastRows] = await DB.query<any[]>(
-    `SELECT ${strengthExerciseColumns}
-    FROM workouts
-    WHERE workouts.exercise_id = ?
-    ORDER BY created_on DESC
-    LIMIT 1`,
-    [exerciseID]
-  );
-
-  return {
-    best: bestRows.length ? castToStrengthExerciseStatItem(bestRows[0]) : null,
-    last: lastRows.length ? castToStrengthExerciseStatItem(lastRows[0]) : null,
-  } as StrengthExerciseStats;
-};
-
-const getCardioExerciseStats = async (exerciseID: number) => {
-  const [bestRows] = await DB.query<any[]>(
-    `SELECT ${cardioExerciseColumns}
-    FROM workouts
-    WHERE exercise_id = ?
-    ORDER BY distance DESC
-    LIMIT 1`,
-    [exerciseID]
-  );
-
-  const [lastRows] = await DB.query<any[]>(
-    `SELECT ${cardioExerciseColumns}
-    FROM workouts
-    WHERE workouts.exercise_id = ?
-    ORDER BY created_on DESC
-    LIMIT 1`,
-    [exerciseID]
-  );
-
-  return {
-    best: bestRows.length ? castToCardioExerciseStatItem(bestRows[0]) : null,
-    last: lastRows.length ? castToCardioExerciseStatItem(lastRows[0]) : null,
-  } as CardioExerciseStats;
+  } as ExerciseStatItem;
 };
 
 export const getExerciseStats = async (
   exerciseID: number,
   exerciseTypeID: ExerciseTypeID
 ) => {
+  let orderBy: string;
   if (exerciseTypeID === ExerciseTypeID.STRENGTH) {
-    return getStrengthExerciseStats(exerciseID);
+    orderBy = strengthOrderBy;
+  } else if (exerciseTypeID === ExerciseTypeID.CARDIO) {
+    orderBy = cardioOrderBy;
+  } else {
+    // Unhandled type
+    orderBy = "id DESC";
   }
 
-  if (exerciseTypeID === ExerciseTypeID.CARDIO) {
-    return getCardioExerciseStats(exerciseID);
-  }
+  const [bestRows] = await DB.query<any[]>(
+    `SELECT ${exerciseStatsColumns}
+    FROM workouts
+    WHERE workouts.exercise_id = ?
+    ORDER BY ${orderBy}
+    LIMIT 1`,
+    [exerciseID]
+  );
 
-  return null;
+  const [lastRows] = await DB.query<any[]>(
+    `SELECT ${exerciseStatsColumns}
+    FROM workouts
+    WHERE workouts.exercise_id = ?
+    ORDER BY created_on DESC
+    LIMIT 1`,
+    [exerciseID]
+  );
+
+  return {
+    best: bestRows.length ? castToExerciseStatItem(bestRows[0]) : null,
+    last: lastRows.length ? castToExerciseStatItem(lastRows[0]) : null,
+  } as ExerciseStats;
 };
--- types/exercise_stats.ts
+++ types/exercise_stats.ts
@@ -1,25 +1,14 @@
-export type ExerciseStats = StrengthExerciseStats | CardioExerciseStats;
-
-export type StrengthExerciseStatItem = {
-  weight: number;
-  reps: number;
-  sets: number;
+export type ExerciseStatItem = {
+  weight: number | null;
+  reps: number | null;
+  sets: number | null;
+  distance: number | null;
+  durationSeconds: number | null;
+  incline: number | null;
   date: string;
 };
 
-export type StrengthExerciseStats = {
-  best: StrengthExerciseStatItem | null;
-  last: StrengthExerciseStatItem | null;
-};
-
-export type CardioExerciseStatItem = {
-  distance: number;
-  durationSeconds: number;
-  incline: number;
-  date: string;
-};
-
-export type CardioExerciseStats = {
-  best: CardioExerciseStatItem | null;
-  last: CardioExerciseStatItem | null;
+export type ExerciseStats = {
+  best: ExerciseStatItem | null;
+  last: ExerciseStatItem | null;
 };
